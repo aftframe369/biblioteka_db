7159b26342dcdcce77a7c4726f4f9cc30f178ac5
maciej <m4.glinski@gmail.com>
2024-10-24 16:23:13 +0200

    init

--- Schema.sql
+++ Schema.sql
@@ -1,2 +1,20 @@
-create database
-create table books
+create DATABASE biblioteka_lodzgorna;
+use biblioteka_lodzgorna;
+
+CREATE TABLE ksiazki ();
+
+CREATE TABLE autorzy ();
+
+CREATE TABLE kategoria ();
+
+CREATE TABLE podkategoria ();
+
+CREATE TABLE miejsce ();
+
+CREATE TABLE osoba ();
+
+CREATE TABLE uzytkownik ();
+
+CREATE TABLE bibliotekarz ();
+
+

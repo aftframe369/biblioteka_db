465ce129538a9bded21c9b79bb193ea90d39078f
Maciek <maciej.glinski@zhp.net.pl>
2024-10-24 21:15:12 +0200

    dfkianwgfun

--- Schema.sql
+++ Schema.sql
@@ -1,63 +1,74 @@
 create DATABASE biblioteka_lodzgorna;
 use biblioteka_lodzgorna;
 
+
 CREATE TABLE egzemplarz (
-	Id varchar(12);
+	CONSTRAINT Foreign Key 
 ); 
 
 CREATE TABLE tytul (
-	Id varchar(12);
-	Tytul varchar(127);
-	Rok YEAR;
-	Ilosc_stron int;
+	Id varchar(12),
+	Tytul varchar(127),
+	Rok YEAR,
+	Ilosc_stron int,
 
-	foreign key wydawnictwo;
-	foreign key kategoria;
-	foreign key pion;
+	wydawnictwo varchar(4).
+
+	foreign key wydawnictwo,
+	foreign key kategoria,
+	foreign key pion,
 );
 
 CREATE TABLE autorzy (
-	Id varchar(5);
-	Imie varchar(20);
-	Nazwisko varchar(100);
-	Uwagi varchar(255);
+	Id varchar(5),
+	Imie varchar(20),
+	Nazwisko varchar(100),
+	Uwagi varchar(255),
 );
 
 CREATE TABLE kategoria (
-	Id varchar(2);
-	Nazwa varchar(30);
+	Id varchar(2),
+	Nazwa varchar(30),
+	Pion varchar(20),
 
-	foreign key id_pionu();
+	foreign key Pion references pion(ID),
 );
 
 CREATE TABLE wydawnictwo (
-	Id varchar(4);
+	Id varchar(4),
 );
 
-CREATE TABLE pion ();
 
 CREATE TABLE podkategoria (
-	Id	varchar(4);
-	Nazwa varchar(125);
+	Nazwa varchar(125) unique,
 
-	foreign key Kategoria();
+	foreign key Kategoria(),
+
+	unique(kategoria, nr_podkategorii),
+	primary key(kategoria, nr_podkategorii),
+
+
 
 );
 
+CREATE TABLE pion (),
+
 CREATE TABLE miejsce (
-	nazwa varchar(20);
+	nazwa varchar(20),
 );
 
 CREATE TABLE osoba (
-	id varchar(4);
-	nr_ewidencji varchar(sprawdzić);
+	id varchar(4),
+	nr_ewidencji varchar(sprawdzić),
 	przydzial
 	imie
 	nazwisko
 );
 
 CREATE TABLE uzytkownik (
-	id_osoby
+	data_rejestracji DATE,
+
+
 );
 
 CREATE TABLE bibliotekarz (
